8c814fbf249cd5ded4a671cd2b50c0e4c5a8ba5d
OrtegaJulio-FS <jcortega1@student.fullsail.edu>
2025-09-17 00:20:54 -0500

    updates site

--- static-site/src/pages/post.js
+++ static-site/src/pages/post.js
@@ -2,18 +2,19 @@ import React from "react";
 import { useParams, Link } from "react-router-dom";
 import posts from "../posts/posts.json";
 import "../index.css";
+import practiceApiImg from "../images/practice-api.png";
 
 export default function Post() {
   const { id } = useParams();
   const post = posts.find((p) => p.id === parseInt(id));
+  const imageMap = {
+    "practice-api.png": practiceApiImg,
+  };
 
   return (
     <div className="container">
       <h1 className="post-title">{post.title}</h1>
-      {post.image && (
-        <img src={post.image} alt={post.title} className="post-image" />
-      )}
-
+      <img src={imageMap[post.image]} alt={post.title} className="post-image" />
       <p className="post-content">{post.content}</p>
 
       <Link to="/" className="back-link">
