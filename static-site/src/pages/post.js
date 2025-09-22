33c9244ca2f63d76a8e65d68e7e93eb8a3a1c431
OrtegaJulio-FS <jcortega1@student.fullsail.edu>
2025-09-22 16:20:35 -0500

    re built

--- static-site/src/pages/post.js
+++ static-site/src/pages/post.js
@@ -3,12 +3,14 @@ import { useParams, Link } from "react-router-dom";
 import posts from "../posts/posts.json";
 import "../index.css";
 import practiceApiImg from "../images/practice-api.png";
+import reactImg from "../images/react-image.jpeg";
 
 export default function Post() {
   const { id } = useParams();
   const post = posts.find((p) => p.id === parseInt(id));
   const imageMap = {
     "practice-api.png": practiceApiImg,
+    "react-image.jpeg": reactImg,
   };
 
   return (
